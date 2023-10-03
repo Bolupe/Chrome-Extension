eb8453ceea5ce2cfe376a7f6eadb8e7a2fa52ad0
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-10-04 00:07:23 +0100

    proper code documentation and read me file

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
-  if(changeInfo.status === "complete" && /^http/.test(tab.url)){
+// Add an event listener for tab updates
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    // Check if the tab's status is "complete" (page finished loading) and if the URL starts with "http"
+    if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+      // Inject a content script into the tab
       chrome.scripting.executeScript({
-          target: {tabId},
-          files: ["./content.js"]
-      }).then(()=>{
-          console.log("We have injected the content script!");
-      }).catch(err=> console.log(err, "Error in background script line 10"))
-  }
-})
+        // Specify the target tab using its ID
+        target: { tabId },
+        // Specify the script file to inject (content.js)
+        files: ["./content.js"]
+      })
+      .then(() => {
+        // Log a success message to the console if the script injection is successful
+        console.log("Content script has been injected successfully!");
+      })
+      .catch((err) => {
+        // Log any errors that occur during script injection
+        console.error(err, "Error in background script line 10");
+      });
+    }
+  });
+  
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Chrome Extension: Screen Recording & Sharing
+
+## Overview
+
+This Chrome Extension allows users to effortlessly record their screens and share the captured content. With a user-friendly interface, it simplifies the screen recording process, ensuring ease of use.
+
+## Key Features
+
+- **Screen Recording:** Initiate and stop screen recording with clear controls.
+- **Content Sharing:** Submit recorded content to a designated online endpoint.
+- **User-Friendly:** Intuitive navigation and controls.
+
+## Get Started
+
+1. Download and install the Chrome Extension.
+2. Follow on-screen instructions to record and share your screen with ease.
+
+
+
+still in the works, would love to make it better soon enough...
+
